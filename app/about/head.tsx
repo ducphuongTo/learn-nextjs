b3fd3e9f9dc9bb5f3d6cd9dbb07379dbf97a10ca
Logan <toducphuong7900@gmail.com>
2023-09-06 00:11:02 +0700

    Routing, Page and layout

---
+++ app/about/head.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+    <>
+        <title>About Page</title>
+        <meta content="width=device-width, initial-scale=1" name='viewport'></meta>
+        <meta  name='description' content='Create by Phuong'></meta>
+        <link rel='icon' href='/favicon.ico'></link>
+    </>
+  )
+}
---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/link'
+export default function About() {
+  return (
+    <>
+        <Link href="/">Go to home page</Link>
+        <div>About Page</div>
+    </>
+  )
+}

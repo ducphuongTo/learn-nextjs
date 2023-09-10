57965be339fd3fe4af3c9db8e92c0f8ceb62758b
Phuong To <phuong.toduc@nastechglobal.com>
2023-09-10 09:52:54 +0700

    apply tailwind css and practice

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Link from 'next/link'
+import { Inter } from "next/font/google"
+const inter = Inter({ subsets: ['latin']})
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-        <h1>Home Page</h1>
-        <p>
-          <Link href="/users">Users</Link>
-        </p>
-    </main>
+    <main className={inter.className}></main>
   )
 }

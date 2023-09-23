cdecad2c75e99601408bd2a700a4d72085926faa
Phuong To <phuong.toduc@nastechglobal.com>
2023-09-24 00:06:09 +0700

    Middleware practice

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,7 +1,21 @@
 import { NextResponse } from "next/server"
+import { limiter } from "../config/limiter";
 
-
-export async function GET() {
+export async function GET(request: Request) {
+    const remaining = await limiter.removeTokens(1)
+    const origin = request.headers.get('origin')
+    console.log('remain:', remaining)
+    if (remaining < 1)
+    {
+        return new NextResponse(null, {
+            status: 429,
+            statusText: "too many requests",
+            headers: {
+                'Access-Control-Allow-Origin': '*' || origin,
+                'Content-Type': 'text/plain'
+            }
+        })
+    }
     const res = await fetch('https://jsonplaceholder.typicode.com/todos', {
         headers: {
             'Content-Type': 'application/json',
